b00a600efd0a7446d646864d06683ad9ea699a4c
Ranika Williams <rnkprice07@gmail.com>
2019-08-09 14:50:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters ORDER BY length(characters.motto) DESC
-   LIMIT 1; "
+   LIMIT 1;"
 end
 
 
