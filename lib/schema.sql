8b948148d2b251096526ad2d3efbf468958a16f3
Ranika Williams <rnkprice07@gmail.com>
2019-08-09 13:39:24 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -15,16 +15,11 @@ CREATE TABLE authors(
   name TEXT
 );
 
-
-CREATE TABLE characters(
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  motto TEXT,
-  species TEXT
-  );
-
 CREATE TABLE books(
   id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 
