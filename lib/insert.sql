7a4254f5bc5052f3491bb6b66c82aee2d4876b2d
Ranika Williams <rnkprice07@gmail.com>
2019-08-09 13:49:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO series (title, author_id, subgenre_id) 
 VALUES ("Harry Potter", 1, 1);
 
-INSERT INTO series () 
-VALUES ();
+INSERT INTO series (title, author_id, subgenre_id) 
+VALUES ("Twilight", 2, 2);
 
